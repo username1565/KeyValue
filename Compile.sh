a912314b65e979062fdb22c6787b0f87a1412c46
username1565 <39200328+username1565@users.noreply.github.com>
2022-08-02 07:54:20 +0300

    Add in memory key-value storage - Hashtable
    Add in memory key-value storage - Hashtable

---
+++ Compile.sh
@@ -0,0 +1,1 @@
+csc /out:Program.exe *.cs
---
+++ Program.cs
@@ -0,0 +1,17 @@
+using Storage;	//include namespace Storage, with KeyValue-class
+
+namespace Program
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			KeyValue.Main_(args);
+			
+			KeyValue table = new KeyValue();
+			table.Count(true);
+			table.Add("test", "test", true);
+			table.Count(true);
+		}
+	}
+}
