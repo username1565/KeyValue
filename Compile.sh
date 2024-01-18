0474adf4bc89e2c822439b053eaf3774ebdf9aa0
username1565 <39200328+username1565@users.noreply.github.com>
2024-01-18 03:28:50 +0000

    Update Compile.sh
    fix linux comment marker

--- Compile.sh
+++ Compile.sh
@@ -1,4 +1,4 @@
 csc /out:Program.exe *.cs /reference:Mono.Data.Sqlite.dll /reference:sqlite3.dll /platform:x86
 
-::mono program.exe DBPathway HashTableName KeyName ValueName
+#mono program.exe DBPathway HashTableName KeyName ValueName
 mono Program.exe Hashtable.db3 KeyValue key value
